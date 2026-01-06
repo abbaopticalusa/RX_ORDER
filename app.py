8a0c0a68a06354e886bc97cd945a4985519de4c6
abbaopticalusa <abbaopticalusa@gmail.com>
2026-01-06 12:46:30 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ col1, col2 = st.columns([0.7, 0.3]) # 70%는 텍스트, 30%는 이미지 공간 
 
 with col1:
     st.title("Abba Optical RX Order App")
-    st.caption("1. Fill out each line as needed. Per eye (R and L). \n 2. When finished, generate the Excel file at the bottom and download it. \n 3. Email the Excel file to order@abbaopticalusa.com. \n\n *Use the Notes column for any special requests (e.g., Mirror Tint, Base Curve, etc.) \n *Please advise FT28 is only available in the options listed in the table.")
+    st.caption("1. Fill out each line as needed. Per eye (R and L). \n 2. When finished, generate the Excel file at the bottom and download it. \n 3. Email the Excel file to order@abbaopticalusa.com. \n\n *Use the Notes column for any special requests (e.g., Mirror Tint, Base Curve, etc.) \n FT28 availability is below \n CR39 \n CLEAR \n 1.56 \n CLEAR \n BLUE STOP \n PHOTO (GREY, BROWN) \n POLY \n CLEAR")
 
 opts = load_options()
 if not opts:
